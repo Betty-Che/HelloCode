29737152c4d676d56dc6846881a1cef381e58320
Черепанова Элизабет Юрьевна <betty.che2806@gmail.com>
2023-01-26 16:19:13 +0500

    Add Example005_IF_Else

---
+++ Example005_IF_Else/Program.cs
@@ -0,0 +1,12 @@
+﻿Console.Write("Введите Ваше имя: ");
+string usrename = Console.ReadLine();
+
+if (usrename.ToLower() == "маша")
+{
+    Console.WriteLine("Ура, это же Маша!");
+}
+else
+{
+    Console.Write("Привет, ");
+    Console.WriteLine(usrename);
+}
